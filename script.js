b7cec835d0d248513c0d0fd30bba7a57673ef87d
JaffetR <jaffetchanco10@gmail.com>
2023-07-30 19:08:19 -0500

    fin de este primer curso

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-(() => {
+
+import checkComplete from "./components/checkComplete.js"
+import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector("[data-form-btn]");
 
@@ -10,34 +12,20 @@ const createTask = (evento) => {
     const task = document.createElement("li");
     task.classList.add('card');
     input.value= "";
-    
-    const taskContent = document.createElement("div");
-    taskContent.appendChild(checkComplete());
+
+    const taskContent = document.createElement("div"); 
     const titleTask = document.createElement("span");
     titleTask.classList.add("task");
     titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    const content = `
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
     //task.innerHTML = content;
+
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
 }
 //Arrow functions o funciones anonimas
 btn.addEventListener("click", createTask);
-
-const checkComplete = () =>{
-    const i = document.createElement("i");
-    i.classList.add("far","fa-check-square","icon");
-    i.addEventListener("click",completeTask);
-    return i;
-}
-
-const completeTask = (event) =>{
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-}
-})();
 
